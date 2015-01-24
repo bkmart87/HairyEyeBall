3820e40a83119b354caa15e7a497207fb8aaa085
Bethany <bkmart87@gmail.com>
2015-01-24 05:07:02 -0800

    Camera Follow
    FEVER RRRRR

---
+++ HairyEyeBall/Assets/CamerFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamerFollow : MonoBehaviour 
+{
+	[SerializeField] 
+	Transform playerTransform;
+	[SerializeField]
+		Vector3 offset;
+
+ void LateUpdate()
+	{
+		transform.position = playerTransform.position + offset;
+
+	}
+
+}
